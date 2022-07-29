dc778e32a25124afd8ce2b23a98f6b0defd1ffb0
Prrfifnmg <prrf@aluno.ifnmg.edu.br>
2022-07-28 21:52:21 -0300

    3 files modified

--- ctr03/ctr03.js
+++ ctr03/ctr03.js
@@ -1,7 +1,7 @@
 function contar() {
   let saida = document.getElementById('saida')
 
-  saida.innerHTML += `<h2>Contando de 10 a 30: </h2>`
+  saida.innerHTML += "<h2>Contando de 10 a 30: </h2>"
   let cont = 10
   while (cont <= 30) {
       saida.innerHTML += ` ${cont} &#x1F449;`
